d45167df2ce9505dabfcfc68e2f87959e0d5e807
William Budoyo <williambu07@gmail.com>
2018-08-17 14:33:47 -0400

    buttons show UPgit add .

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const burger = require("../model/burger.js");
 
 router.get("/", function(req,res){
+  
   burger.selectAll(function(data){
+      // we need to create obj here so we can use #each method from handlebars.
         let burgersObj = {
             burgers: data
         };
