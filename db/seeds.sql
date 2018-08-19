3d4a73b7adaa62740cc4aa76ca3bd575e52723fb
William Budoyo <williambu07@gmail.com>
2018-08-19 09:42:16 -0400

    push again

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
 INSERT INTO burgers (burger_name) VALUES ("Double Cheese Burger");
 INSERT INTO burgers (burger_name, devoured) VALUES("Hamburger", true);
